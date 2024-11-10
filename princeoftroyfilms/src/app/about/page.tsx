3cc9b87519bc30610663c51e08b24984eddcc67b
princeoftroyfilms <jasonwarman18@gmail.com>
2024-11-10 03:03:07 +0000

    Update page.tsx

--- princeoftroyfilms/src/app/about/page.tsx
+++ princeoftroyfilms/src/app/about/page.tsx
@@ -3,7 +3,7 @@
 import { useRef, useState } from 'react'
 import Image from 'next/image'
 import { motion, useScroll, useTransform } from 'framer-motion'
-import Navigation from '@/components/navigation'
+import Navigation from 'src/app/components/navigation'
 
 export default function AboutPage() {
   const containerRef = useRef<HTMLDivElement>(null)
